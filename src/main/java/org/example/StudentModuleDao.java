54d192ba28e802f2db0a860e8d213ff2c5e07246
Outassla Ilyas <101256242+ily1s@users.noreply.github.com>
2025-03-12 19:36:42 +0000

    Update StudentModuleDao.java

--- src/main/java/org/example/StudentModuleDao.java
+++ src/main/java/org/example/StudentModuleDao.java
@@ -9,23 +9,18 @@ public class StudentModuleDao {
 
         em.getTransaction().begin();
         try {
-            // Create a student
             Student student = new Student("John Doe");
 
-            // Create modules
             Module module1 = new Module("Mathematics");
             Module module2 = new Module("Computer Science");
 
-            // Add modules to the student
             student.addModule(module1);
             student.addModule(module2);
 
-            // Persist the student (modules will be cascaded)
             em.persist(student);
 
             em.getTransaction().commit();
 
-            // Retrieve the student and print details
             Student retrievedStudent = em.find(Student.class, student.getId());
             System.out.println("Student: " + retrievedStudent.getName());
             System.out.println("Modules:");
@@ -33,9 +28,7 @@ public class StudentModuleDao {
                 System.out.println("- " + module.getName());
             }
         } catch (Exception e) {
-            if (em.getTransaction().isActive()) {
-                em.getTransaction().rollback();
-            }
+            em.getTransaction().rollback();
             e.printStackTrace();
         } finally {
             em.close();
